93ebff1d2253aefe637c9c5e24d22d1b4e627dc8
Silvia Almeida <sa@linkfire.com>
2022-05-22 19:42:29 +0100

    Initial commit

---
+++ src/components/Row.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function Row({ guess, currentGuess }) {
+  if (guess) {
+    return (
+      <div className="row past">
+        {guess.map((letter, i) => (
+          <div key={i} className={letter.color}>
+            {letter.key}
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  if (currentGuess) {
+    let letters = currentGuess.split("");
+    return (
+      <div className="row current">
+        {letters.map((letter, i) => (
+          <div key={i} className="filled">{letter}</div>
+        ))}
+        {[...Array(5 - letters.length)].map((_, i) => (
+          <div key={i}></div>
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div className="row">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
